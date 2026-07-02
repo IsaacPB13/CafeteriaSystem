a97e39045c56f3d11012fd5fd9c1b63b032e26b2
EmanuelC <santiago.carino05@gmail.com>
2026-07-02 11:38:32 -0600

    Agrego clase Menu

--- src/Menu.java
+++ src/Menu.java
@@ -1,4 +1,29 @@
+import java.util.HashMap;
+
 public class Menu {
+    private HashMap<String, Producto> productos;
+
+    public Menu() {
+        productos = new HashMap<>();
+    }// Menu
+
+    public void agregarProducto(Producto producto) {
+        productos.put(producto.getNombre(), producto);
+    }// agrega un producto al menú
 
 
-}
+    public void mostrarMenu() {
+
+        System.out.println("========== MENÚ ==========");
+
+        for (Producto producto : productos.values()) {
+            producto.mostrarInformacion();
+        }// for
+    }// muestra todos los productos del menú
+
+
+    public Producto buscarProducto(String nombre) {
+        return productos.get(nombre);
+    }// busca un producto por nombre
+
+}// Class Menu
