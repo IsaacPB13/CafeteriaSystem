14ead6dccd02c5a89f0cda1dee07ea5b9a6e01b5
RodrigoFlores <rodrigoflores29061997@gmail.com>
2026-07-02 12:07:46 -0600

    Agrego Bebida y Alimento

--- src/Alimento.java
+++ src/Alimento.java
@@ -1,4 +1,41 @@
-public class Alimento {
+public class Alimento extends Producto implements Vendible {
 
+    private boolean vegetariano;
+    private int calorias;
 
+    public Alimento(String nombre, double precio, boolean disponible,
+                    boolean vegetariano, int calorias) {
+
+        super(nombre, precio, disponible);
+        this.vegetariano = vegetariano;
+        this.calorias = calorias;
+    }
+
+    public boolean isVegetariano() {
+        return vegetariano;
+    }
+
+    public void setVegetariano(boolean vegetariano) {
+        this.vegetariano = vegetariano;
+    }
+
+    public int getCalorias() {
+        return calorias;
+    }
+
+    public void setCalorias(int calorias) {
+        this.calorias = calorias;
+    }
+
+    @Override
+    public void mostrarinformacion() {
+        super.mostrarinformacion();
+        System.out.println("Vegetariano: " + (vegetariano ? "Sí" : "No"));
+        System.out.println("Calorías: " + calorias);
+    }
+
+    @Override
+    public double calcularPrecioFinal() {
+        return getPrecio();
+    }
 }
--- src/Bebida.java
+++ src/Bebida.java
@@ -1,4 +1,41 @@
-public class Bebida {
+public class Bebida extends Producto implements Vendible {
 
+    private String tamano;
+    private boolean fria;
 
+    public Bebida(String nombre, double precio, boolean disponible,
+                  String tamano, boolean fria) {
+
+        super(nombre, precio, disponible);
+        this.tamano = tamano;
+        this.fria = fria;
+    }
+
+    public String getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(String tamano) {
+        this.tamano = tamano;
+    }
+
+    public boolean isFria() {
+        return fria;
+    }
+
+    public void setFria(boolean fria) {
+        this.fria = fria;
+    }
+
+    @Override
+    public void mostrarinformacion() {
+        super.mostrarinformacion();
+        System.out.println("Tamaño: " + tamano);
+        System.out.println("Temperatura: " + (fria ? "Fría" : "Caliente"));
+    }
+
+    @Override
+    public double calcularPrecioFinal() {
+        return getPrecio();
+    }
 }
