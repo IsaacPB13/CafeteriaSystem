3d8dba3204daecff91e48dd36b46e9b9aaffe2da
IsaacPB13 <iperezb1308@gmail.com>
2026-07-02 10:53:18 -0600

    Agregando los marcos al repositorio

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String args[]){
+
+        //Primero se despliega menu
+
+        //Seguido se permiten añadir productos a la orden
+
+        //Cuando se termine de añadir, se muestra el resultado de la orden y el precio
+    }
+}
